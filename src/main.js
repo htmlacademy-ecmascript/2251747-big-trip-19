16d918d146cfa516ebee92dc7be3dfd989979bf0
Aleksandra Ochirova <ochirova.alex@gmail.com>
2022-12-07 20:51:50 +0400

    Время разбивать камни #3(исправила)

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
-//import NewEventButtonView from './view/new-event-button-view.js';
-//import FilterView from './view/filter-view.js';
-import BoardPresenter from './presenter/board-presenter.js';
+import Presenter from './presenter/presenter.js';
 
 
 const siteMainElement = document.querySelector('.trip-main');
 const siteHeaderElement = document.querySelector('.trip-events');
-const boardPresenter = new BoardPresenter({boardContainer: siteMainElement, bodyContainer: siteHeaderElement});
+const boardPresenter = new Presenter({boardContainer: siteMainElement, bodyContainer: siteHeaderElement});
 
 boardPresenter.init();
--- src/presenter/presenter.js
+++ src/presenter/presenter.js
@@ -1,14 +1,14 @@
 import BoardView from '../view/board-view.js';
-import FilterBorder from '../view/filter-border.js';
+import FilterView from '../view/filter-view.js';
 import EventItemView from '../view/event-item';
-import {RenderPosition, render,} from '../render.js';
+import {RenderPosition, render} from '../render.js';
 import EventEditList from '../view/event-edit-item.js';
 import SortView from '../view/sort-view.js';
 import EventsListView from '../view/events-list.js';
 
-export default class BoardPresenter {
+export default class Presenter {
   boardComponent = new BoardView();
-  filterComponent = new FilterBorder();
+  filterComponent = new FilterView();
   sortComponent = new SortView();
   eventEditListComponent = new EventEditList();
   eventsList = new EventsListView();
@@ -24,7 +24,6 @@ export default class BoardPresenter {
     render(this.sortComponent, this.bodyContainer);
     render(this.eventsList, this.bodyContainer);
     render(this.eventEditListComponent, this.eventsList.getElement());
-    // render(new EventEditView(), this.taskListComponent.getElement());
 
     for (let i = 0; i < 3; i++) {
       render(new EventItemView(), this.eventsList.getElement());
